89da8c7ebccf5f57fc025e13e5c1a9d16c6562dd
Nr0ker <125687629+Nr0ker@users.noreply.github.com>
2023-06-15 20:10:59 +0200

    no html

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 import pygame
 print("dance dance dance with my hands")
 print(1)
-<<<<<<< HEAD
 print(1)
-
 def bb():
     pass
-=======
 # изменения
-
-
 def while_print():
     user = input("""Ваш текс: """)
-
 while_print()
-
->>>>>>> a4b124f5fa46e945c2313f77185df87f664b2877
