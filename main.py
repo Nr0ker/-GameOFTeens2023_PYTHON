a4b124f5fa46e945c2313f77185df87f664b2877
Nr0ker <125687629+Nr0ker@users.noreply.github.com>
2023-06-15 20:08:28 +0200

    Changes by Vladislav

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-print(1)
+import pygame
+print("dance dance dance with my hands")
 print(1)
 # изменения
 
-def abs():
-    pass
+
+def while_print():
+    user = input("""Ваш текс: """)
+
+while_print()
+
