5e67371c0e222f1b8d53640589a6c3fc94c3695b
Nr0ker <125687629+Nr0ker@users.noreply.github.com>
2023-06-16 15:58:22 +0200

    update

--- start.py
+++ start.py
@@ -5,3 +5,5 @@ from first_kb import kb
 @dp.message_handler(commands=['start'])
 async def bot_start(message: types.Message):
     await message.answer(f"Доброго дня, {message.from_user.full_name}!\n ", reply_markup=kb)
+
+
