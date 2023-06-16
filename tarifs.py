eae9c6ba8dd6fbefba6a168502305f280d1fbbc4
Nr0ker <125687629+Nr0ker@users.noreply.github.com>
2023-06-16 15:58:28 +0200

    update

---
+++ tarifs.py
@@ -0,0 +1,16 @@
+from aiogram import types
+from loader import dp, bot
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.dispatcher import FSMContext
+import re
+regex_email = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+regex_password = re.compile(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')
+
+tarifs = ["Вільний лайф", "Смарт лайф", "Просто лайф", "Platinum Лайф", "Шкільний лайф", "Ґаджет", "Смарт сім'я"]
+
+
+class TarifStatesGroup(StatesGroup):
+
+    one = State()
+    two = State()
+# inline butt
